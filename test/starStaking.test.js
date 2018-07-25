b4d509ba00b3295c9edb342d11d38f8ff9f0729f
Markus <mail@markuswaas.de>
2018-07-25 20:42:43 +0200

    329 test staking points computation
    * use different amounts to test the computation
    * minor fluctuations in blockchain time must be considered
    * also refactor other tests to changes

--- test/starStaking.test.js
+++ test/starStaking.test.js
@@ -1,16 +1,34 @@
+const BigNumber = require('bignumber.js');
+
 const StarStaking = artifacts.require('StarStaking.sol');
 const MintableToken = artifacts.require('MintableToken.sol');
 
 const { should } = require('./helpers/utils');
 const { increaseTimeTo, latestTime } = require('./helpers/timer');
 
+const BALANCES = [
+    new BigNumber(10),
+    new BigNumber(200),
+    new BigNumber(4000),
+    new BigNumber(10000),
+    new BigNumber(60000),
+    new BigNumber(200000),
+    new BigNumber(3333333),
+    new BigNumber(44444444),
+    new BigNumber(999999999),
+    new BigNumber(1000000000),
+];
+
 contract('StarStaking', function([user, user2]) {
-    let stakingContract, token, initialBalance;
+    let stakingContract, token, initialBalance, openingTime, closingTime;
 
     beforeEach(async () => {
-        initialBalance = 10000;
+        initialBalance = new BigNumber(1000000000);
         token = await MintableToken.new();
-        stakingContract = await StarStaking.new(token.address);
+       
+        openingTime = new BigNumber(latestTime()).plus(1000);
+        closingTime = openingTime.plus(200000);
+        stakingContract = await StarStaking.new(token.address, openingTime, closingTime);
 
         await token.mint(user, initialBalance);
         await token.approve(stakingContract.address, initialBalance, {
@@ -18,105 +36,70 @@ contract('StarStaking', function([user, user2]) {
         });
     });
 
-    it('transfers tokens to stakingContract when staked', async () => {
-        await stakingContract.stake(initialBalance);
-        const userBalance = await token.balanceOf.call(user);
-        const stakingContractBalance = await token.balanceOf.call(
-            stakingContract.address
-        );
-
-        userBalance.should.be.bignumber.equal(0);
-        stakingContractBalance.should.be.bignumber.equal(initialBalance);
-    });
-
-    it('allows user to unstake tokens', async () => {
-        await stakingContract.stake(initialBalance);
-
-        const userTotalStaked = await stakingContract.totalStakedFor.call(user);
-        userTotalStaked.should.be.bignumber.equal(initialBalance);
-
-        await stakingContract.unstake(initialBalance / 2);
-
-        const userNewTotalStake = await stakingContract.totalStakedFor.call(
-            user
-        );
-        userNewTotalStake.should.be.bignumber.equal(initialBalance / 2);
-    });
-
-    it('allows user to stake for other person', async () => {
-        await stakingContract.stakeFor(user2, initialBalance, { from: user });
-
-        const user2TotalStaked = await stakingContract.totalStakedFor.call(
-            user2
-        );
-        user2TotalStaked.should.be.bignumber.equal(initialBalance);
-
-        await stakingContract.unstake(initialBalance / 2, { from: user2 });
-
-        const user2NewTotalStake = await stakingContract.totalStakedFor.call(
-            user2
-        );
-        user2NewTotalStake.should.be.bignumber.equal(initialBalance / 2);
-    });
-
-    describe('staking constants', async () => {
-        let firstBlock;
-        let secondBlock;
-
+    describe('staking period is open', async () => {
         beforeEach(async () => {
-            let result = await stakingContract.stake(initialBalance / 2);
-            firstBlock = result['receipt']['blockNumber'];
-
-            await increaseTimeTo(latestTime() + 5);
-
-            result = await stakingContract.stake(initialBalance / 2);
-            secondBlock = result['receipt']['blockNumber'];
+            await increaseTimeTo(openingTime);
         });
 
-        it('returns full staked value when calling totalStaked', async () => {
-            const userTotalStaked = await stakingContract.totalStakedFor.call(
-                user
+        it('transfers tokens to stakingContract when staked', async () => {
+            await stakingContract.stake(initialBalance);
+            const userBalance = await token.balanceOf.call(user);
+            const stakingContractBalance = await token.balanceOf.call(
+                stakingContract.address
             );
-            userTotalStaked.should.be.bignumber.equal(initialBalance);
+
+            userBalance.should.be.bignumber.equal(0);
+            stakingContractBalance.should.be.bignumber.equal(initialBalance);
         });
 
-        it('returns correct amount staked at block', async () => {
-            const userTotalStakedAtBlock = await stakingContract.totalStakedForAt.call(
-                user,
-                firstBlock
-            );
-            userTotalStakedAtBlock.should.be.bignumber.equal(
-                initialBalance / 2
-            );
-        });
+        it('allows user to stake for other person', async () => {
+            await stakingContract.stakeFor(user2, initialBalance, { from: user });
 
-        it('returns correct block when calling lastStaked', async () => {
-            const userLastStaked = await stakingContract.lastStakedFor.call(
-                user
+            const user2TotalStaked = await stakingContract.totalStakedFor.call(
+                user2
             );
-            userLastStaked.should.be.bignumber.equal(secondBlock);
-        });
-
-        it('returns correct amount staked at block in future', async () => {
-            const userTotalStakedAtBlock = await stakingContract.totalStakedForAt.call(
-                user,
-                secondBlock * 2
-            );
-            userTotalStakedAtBlock.should.be.bignumber.equal(initialBalance);
-        });
+            user2TotalStaked.should.be.bignumber.equal(initialBalance);
+        });           
     });
 
-    it('returns correct total amount staked', async () => {
-        await stakingContract.stake(initialBalance / 2, { from: user });
-        let result = await stakingContract.stake(initialBalance / 2, {
-            from: user
+    function computeStakingPoints({ amount, timeAdvanced }) {
+        let currentTime = new BigNumber(latestTime());
+        currentTime = timeAdvanced ? currentTime.minus(1) : currentTime;
+        const timeUntilEnd = closingTime.minus(currentTime);
+        const stakingPoints = timeUntilEnd.times(amount);
+
+        return stakingPoints;
+    }
+
+    async function evaluateComputation(amount) {
+        await stakingContract.stake(amount);
+        const userTotalStakingPoints = await stakingContract.totalStakingPointsFor.call(user);
+
+        const pointsWithoutTimeAdvanced = computeStakingPoints({ amount, timeAdvanced: false });
+        const pointsWithTimeAdvanced = computeStakingPoints({ amount, timeAdvanced: true });
+
+        userTotalStakingPoints.should.be.bignumber.at.least(pointsWithoutTimeAdvanced);
+        userTotalStakingPoints.should.be.bignumber.at.most(pointsWithTimeAdvanced);
+    }
+
+    describe(`adding new stake`, async () => {
+        BALANCES.forEach(async balance => {
+            describe(`staking ${balance.toNumber()} tokens`, async () => {
+                it('calculates the points correctly at the beginning', async () => {
+                    await increaseTimeTo(openingTime);
+                    evaluateComputation(balance);
+                });
+        
+                it('calculates the points correctly in the middle', async () => {
+                    await increaseTimeTo(openingTime.plus(closingTime.minus(openingTime).div(2)));
+                    evaluateComputation(balance);
+                });
+        
+                it('calculates the points correctly at the end', async () => {
+                    await increaseTimeTo(closingTime.minus(20));
+                    evaluateComputation(balance);
+                });
+            });
         });
-
-        let block = result['receipt']['blockNumber'];
-
-        const userTotalStakedAt = await stakingContract.totalStakedAt.call(
-            block * 2
-        );
-        userTotalStakedAt.should.be.bignumber.equal(initialBalance);
     });
 });
