5f31039b735cd2a6fb5bf493f5d50a0cfc26192e
Markus <mail@markuswaas.de>
2018-08-01 15:15:28 +0200

    329 remove outdated functions in interface

--- contracts/StarStakingInterface.sol
+++ contracts/StarStakingInterface.sol
@@ -3,7 +3,4 @@ pragma solidity 0.4.24;
 
 contract StarStakingInterface {
     event Staked(address indexed user, uint256 amount, uint256 addedStakingPoints);
-
-    function token() public view returns (address);
-    function supportsHistory() public pure returns (bool);
 }
