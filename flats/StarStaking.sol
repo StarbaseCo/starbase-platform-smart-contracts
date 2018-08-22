f4910ea7b14c8420dc0364c4c22caa247411cf01
Gustavo Guimaraes <gustavoguimaraes@gmail.com>
2018-08-22 12:04:08 +0200

    add flattened contract

---
+++ flats/StarStaking.sol
@@ -0,0 +1,469 @@
+pragma solidity 0.4.24;
+
+// File: contracts/lib/Ownable.sol
+
+/**
+ * @title Ownable
+ * @dev The Ownable contract has an owner address, and provides basic authorization control
+ * functions, this simplifies the implementation of "user permissions".
+ */
+contract Ownable {
+  address public owner;
+
+  event OwnershipRenounced(address indexed previousOwner);
+  event OwnershipTransferred(
+    address indexed previousOwner,
+    address indexed newOwner
+  );
+
+  /**
+   * @dev The Ownable constructor sets the original `owner` of the contract to the sender
+   * account.
+   */
+  constructor() public {
+    owner = msg.sender;
+  }
+
+  /**
+   * @dev Throws if called by any account other than the owner.
+   */
+  modifier onlyOwner() {
+    require(msg.sender == owner);
+    _;
+  }
+
+  /**
+   * @dev Allows the current owner to relinquish control of the contract.
+   * @notice Renouncing to ownership will leave the contract without an owner.
+   * It will not be possible to call the functions with the `onlyOwner`
+   * modifier anymore.
+   */
+  function renounceOwnership() public onlyOwner {
+    emit OwnershipRenounced(owner);
+    owner = address(0);
+  }
+
+  /**
+   * @dev Allows the current owner to transfer control of the contract to a newOwner.
+   * @param _newOwner The address to transfer ownership to.
+   */
+  function transferOwnership(address _newOwner) public onlyOwner {
+    _transferOwnership(_newOwner);
+  }
+
+  /**
+   * @dev Transfers control of the contract to a newOwner.
+   * @param _newOwner The address to transfer ownership to.
+   */
+  function _transferOwnership(address _newOwner) internal {
+    require(_newOwner != address(0));
+    emit OwnershipTransferred(owner, _newOwner);
+    owner = _newOwner;
+  }
+}
+
+// File: contracts/lib/SafeMath.sol
+
+/**
+ * @title SafeMath
+ * @dev Math operations with safety checks that throw on error
+ */
+library SafeMath {
+
+  /**
+  * @dev Multiplies two numbers, throws on overflow.
+  */
+  function mul(uint256 a, uint256 b) internal pure returns (uint256 c) {
+    if (a == 0) {
+      return 0;
+    }
+    c = a * b;
+    assert(c / a == b);
+    return c;
+  }
+
+  /**
+  * @dev Integer division of two numbers, truncating the quotient.
+  */
+  function div(uint256 a, uint256 b) internal pure returns (uint256) {
+    // assert(b > 0); // Solidity automatically throws when dividing by 0
+    // uint256 c = a / b;
+    // assert(a == b * c + a % b); // There is no case in which this doesn't hold
+    return a / b;
+  }
+
+  /**
+  * @dev Subtracts two numbers, throws on overflow (i.e. if subtrahend is greater than minuend).
+  */
+  function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+    assert(b <= a);
+    return a - b;
+  }
+
+  /**
+  * @dev Adds two numbers, throws on overflow.
+  */
+  function add(uint256 a, uint256 b) internal pure returns (uint256 c) {
+    c = a + b;
+    assert(c >= a);
+    return c;
+  }
+}
+
+// File: contracts/lib/ERC20.sol
+
+/**
+ * @title ERC20 interface
+ * @dev see https://github.com/ethereum/EIPs/issues/20
+ */
+contract ERC20 {
+    function allowance(address owner, address spender) public view returns (uint256);
+    function transferFrom(address from, address to, uint256 value) public returns (bool);
+    function approve(address spender, uint256 value) public returns (bool);
+    function totalSupply() public view returns (uint256);
+    function balanceOf(address who) public view returns (uint256);
+    function transfer(address to, uint256 value) public returns (bool);
+
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+    event Transfer(address indexed from, address indexed to, uint256 value);
+}
+
+// File: contracts/Lockable.sol
+
+contract Lockable is Ownable {
+
+    bool public locked;
+
+    modifier onlyWhenUnlocked() {
+        require(!locked);
+        _;
+    }
+
+    function lock() external onlyOwner {
+        locked = true;
+    }
+
+    function unlock() external onlyOwner {
+        locked = false;
+    }
+}
+
+// File: contracts/LinkedListLib.sol
+
+/**
+ * @title LinkedListLib
+ * @author Darryl Morris (o0ragman0o) and Modular.network
+ *
+ * This utility library was forked from https://github.com/o0ragman0o/LibCLL
+ * into the Modular-Network ethereum-libraries repo at https://github.com/Modular-Network/ethereum-libraries
+ * It has been updated to add additional functionality and be more compatible with solidity 0.4.18
+ * coding patterns.
+ *
+ * version 1.0.0
+ * Copyright (c) 2017 Modular Inc.
+ * The MIT License (MIT)
+ * https://github.com/Modular-Network/ethereum-libraries/blob/master/LICENSE
+ *
+ * The LinkedListLib provides functionality for implementing data indexing using
+ * a circlular linked list
+ *
+ * Modular provides smart contract services and security reviews for contract
+ * deployments in addition to working on open source projects in the Ethereum
+ * community. Our purpose is to test, document, and deploy reusable code onto the
+ * blockchain and improve both security and usability. We also educate non-profits,
+ * schools, and other community members about the application of blockchain
+ * technology. For further information: modular.network
+ *
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ * CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+
+library LinkedListLib {
+
+    address constant NULL = 0;
+    address constant HEAD = 0;
+    bool constant PREV = false;
+    bool constant NEXT = true;
+
+    struct LinkedList{
+        mapping (address => mapping (bool => address)) list;
+    }
+
+    /// @dev returns true if the list exists
+    /// @param self stored linked list from contract
+    function listExists(LinkedList storage self)
+        internal
+        view returns (bool)
+    {
+        // if the head nodes previous or next pointers both point to itself, then there are no items in the list
+        if (self.list[HEAD][PREV] != HEAD || self.list[HEAD][NEXT] != HEAD) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /// @dev returns true if the node exists
+    /// @param self stored linked list from contract
+    /// @param _node a node to search for
+    function nodeExists(LinkedList storage self, address _node)
+        internal
+        view returns (bool)
+    {
+        if (self.list[_node][PREV] == HEAD && self.list[_node][NEXT] == HEAD) {
+            if (self.list[HEAD][NEXT] == _node) {
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            return true;
+        }
+    }
+
+    /// @dev Returns the number of elements in the list
+    /// @param self stored linked list from contract
+    function sizeOf(LinkedList storage self) internal view returns (uint256 numElements) {
+        bool exists;
+        address i;
+        (exists,i) = getAdjacent(self, HEAD, NEXT);
+        while (i != HEAD) {
+            (exists,i) = getAdjacent(self, i, NEXT);
+            numElements++;
+        }
+        return;
+    }
+
+    /// @dev Returns the links of a node as a tuple
+    /// @param self stored linked list from contract
+    /// @param _node id of the node to get
+    function getNode(LinkedList storage self, address _node)
+        internal view returns (bool,address,address)
+    {
+        if (!nodeExists(self,_node)) {
+            return (false,0,0);
+        } else {
+            return (true,self.list[_node][PREV], self.list[_node][NEXT]);
+        }
+    }
+
+    /// @dev Returns the link of a node `_node` in direction `_direction`.
+    /// @param self stored linked list from contract
+    /// @param _node id of the node to step from
+    /// @param _direction direction to step in
+    function getAdjacent(LinkedList storage self, address _node, bool _direction)
+        internal view returns (bool,address)
+    {
+        if (!nodeExists(self,_node)) {
+            return (false,0);
+        } else {
+            return (true,self.list[_node][_direction]);
+        }
+    }
+
+    /// @dev Can be used before `insert` to build an ordered list
+    /// @param self stored linked list from contract
+    /// @param _node an existing node to search from, e.g. HEAD.
+    /// @param _value value to seek
+    /// @param _direction direction to seek in
+    //  @return next first node beyond '_node' in direction `_direction`
+
+    // TODO change to reflect _value as totalStakingPoints[_value]
+    function getSortedSpot(LinkedList storage self, address _node, address _value, bool _direction)
+        internal view returns (address)
+    {
+        if (sizeOf(self) == 0) { return 0; }
+        require((_node == 0) || nodeExists(self,_node));
+        bool exists;
+        address next;
+        (exists,next) = getAdjacent(self, _node, _direction);
+        while  ((next != 0) && (_value != next) && ((_value < next) != _direction)) next = self.list[next][_direction];
+        return next;
+    }
+
+    /// @dev Creates a bidirectional link between two nodes on direction `_direction`
+    /// @param self stored linked list from contract
+    /// @param _node first node for linking
+    /// @param _link  node to link to in the _direction
+    function createLink(LinkedList storage self, address _node, address _link, bool _direction) internal  {
+        self.list[_link][!_direction] = _node;
+        self.list[_node][_direction] = _link;
+    }
+
+    /// @dev Insert node `_new` beside existing node `_node` in direction `_direction`.
+    /// @param self stored linked list from contract
+    /// @param _node existing node
+    /// @param _new  new node to insert
+    /// @param _direction direction to insert node in
+    function insert(LinkedList storage self, address _node, address _new, bool _direction) internal returns (bool) {
+        if(!nodeExists(self,_new) && nodeExists(self,_node)) {
+            address c = self.list[_node][_direction];
+            createLink(self, _node, _new, _direction);
+            createLink(self, _new, c, _direction);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /// @dev removes an entry from the linked list
+    /// @param self stored linked list from contract
+    /// @param _node node to remove from the list
+    function remove(LinkedList storage self, address _node) internal returns (address) {
+        if ((_node == NULL) || (!nodeExists(self,_node))) { return 0; }
+        createLink(self, self.list[_node][PREV], self.list[_node][NEXT], NEXT);
+        delete self.list[_node][PREV];
+        delete self.list[_node][NEXT];
+        return _node;
+    }
+
+    /// @dev pushes an entry to the head of the linked list
+    /// @param self stored linked list from contract
+    /// @param _node new entry to push to the head
+    /// @param _direction push to the head (NEXT) or tail (PREV)
+    function push(LinkedList storage self, address _node, bool _direction) internal  {
+        insert(self, HEAD, _node, _direction);
+    }
+
+    /// @dev pops the first entry from the linked list
+    /// @param self stored linked list from contract
+    /// @param _direction pop from the head (NEXT) or the tail (PREV)
+    function pop(LinkedList storage self, bool _direction) internal returns (address) {
+        bool exists;
+        address adj;
+
+        (exists,adj) = getAdjacent(self, HEAD, _direction);
+
+        return remove(self, adj);
+    }
+}
+
+// File: contracts/StarStakingInterface.sol
+
+contract StarStakingInterface {
+    event Staked(address indexed user, uint256 amount, uint256 addedStakingPoints);
+}
+
+// File: contracts/StarStaking.sol
+
+contract StarStaking is StarStakingInterface, Lockable {
+    using SafeMath for uint256;
+    using LinkedListLib for LinkedListLib.LinkedList;
+
+    address constant HEAD = 0x0;
+    bool constant PREV = false;
+    bool constant NEXT = true;
+
+    ERC20 public token;
+
+    mapping (address => uint256) public totalStakingPointsFor;
+    mapping (address => uint256) public totalStakedFor;
+
+    LinkedListLib.LinkedList topRanks;
+    uint256 public topRanksCount;
+
+    uint256 public startTime;
+    uint256 public closingTime;
+
+    modifier whenStakingOpen {
+        require(now >= startTime);
+        require(now < closingTime);
+
+        _;
+    }
+
+    /**
+     * @param _token Token that can be staked.
+     * @param _startTime Timestamp for the beginning of the staking event
+     * @param _closingTime Timestamp of the end of staking event
+     */
+    constructor(ERC20 _token, uint256 _startTime, uint256 _closingTime) public {
+        require(address(_token) != 0x0);
+        require(_startTime < _closingTime);
+        require(_startTime > now);
+
+        token = _token;
+        startTime = _startTime;
+        closingTime = _closingTime;
+        topRanksCount = 0;
+    }
+
+    /**
+     * @dev Stakes a certain amount of tokens.
+     * @param amount Amount of tokens to stake.
+     */
+    function stake(uint256 amount) public {
+        stakeFor(msg.sender, amount);
+    }
+
+    /**
+     * @dev Stakes a certain amount of tokens for another user.
+     * @param user Address of the user to stake for.
+     * @param amount Amount of tokens to stake.
+     */
+    function stakeFor(address user, uint256 amount) public onlyWhenUnlocked whenStakingOpen {
+        addStakingPoints(user, amount);
+
+        require(token.transferFrom(msg.sender, address(this), amount));
+    }
+
+    function addStakingPoints(address user, uint256 amount) internal {
+        uint256 timeUntilEnd = closingTime.sub(now);
+        uint256 addedStakingPoints = timeUntilEnd.mul(amount);
+
+        totalStakingPointsFor[user] = totalStakingPointsFor[user].add(addedStakingPoints);
+        totalStakedFor[user] = totalStakedFor[user].add(amount);
+
+        emit Staked(user, amount, addedStakingPoints);
+    }
+
+    // TODO: Remove for production
+    function fakeInsert(address user, uint256 amount, address referenceNode) public {
+        addStakingPoints(user, amount);
+        topRanks.insert(referenceNode, user, NEXT);
+
+        topRanksCount++;
+    }
+
+    /**
+     * @dev Returns the previous or next top rank node.
+     * @param referenceNode Address of the reference.
+     * @param direction Bool for direction
+     */
+    function getTopRank(address referenceNode, bool direction) public view returns (address) {
+        return topRanks.list[referenceNode][direction];
+    }
+
+    /**
+     * @dev Returns a flat list of 3-tuples (address, stakingPoints, totalStaked).
+     */
+    function getTopRanksTuples() public view returns (uint256[]) {
+        uint256 tripleRanksCount = topRanksCount * 3;
+        uint256[] memory topRanksList = new uint256[](tripleRanksCount);
+
+        address referenceNode = HEAD;
+        uint256 x = 0;
+
+        while(x < tripleRanksCount) {
+            referenceNode = getTopRank(referenceNode, NEXT);
+
+            topRanksList[x] = uint256(referenceNode);
+            x++;
+
+            topRanksList[x] = totalStakingPointsFor[referenceNode];
+            x++;
+
+            topRanksList[x] = totalStakedFor[referenceNode];
+            x++;
+        }
+
+        return topRanksList;
+    }
+}
---
+++ flats/TokenSaleCloneFactory.sol
@@ -0,0 +1,277 @@
+pragma solidity 0.4.24;
+
+// File: contracts/TokenSaleInterface.sol
+
+/**
+ * @title TokenSale contract interface
+ */
+interface TokenSaleInterface {
+    function init
+    (
+        uint256 _startTime,
+        uint256 _endTime,
+        address _whitelist,
+        address _starToken,
+        address _companyToken,
+        uint256 _rate,
+        uint256 _starRate,
+        address _wallet,
+        uint256 _crowdsaleCap
+    )
+    external;
+}
+
+// File: contracts/cloneFactory/CloneFactory.sol
+
+/*
+The MIT License (MIT)
+Copyright (c) 2018 Murray Software, LLC.
+Permission is hereby granted, free of charge, to any person obtaining
+a copy of this software and associated documentation files (the
+"Software"), to deal in the Software without restriction, including
+without limitation the rights to use, copy, modify, merge, publish,
+distribute, sublicense, and/or sell copies of the Software, and to
+permit persons to whom the Software is furnished to do so, subject to
+the following conditions:
+The above copyright notice and this permission notice shall be included
+in all copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+//solhint-disable max-line-length
+//solhint-disable no-inline-assembly
+
+contract CloneFactory {
+
+  event CloneCreated(address indexed target, address clone);
+
+  function createClone(address target) internal returns (address result) {
+    bytes memory clone = hex"600034603b57603080600f833981f36000368180378080368173bebebebebebebebebebebebebebebebebebebebe5af43d82803e15602c573d90f35b3d90fd";
+    bytes20 targetBytes = bytes20(target);
+    for (uint i = 0; i < 20; i++) {
+      clone[26 + i] = targetBytes[i];
+    }
+    assembly {
+      let len := mload(clone)
+      let data := add(clone, 0x20)
+      result := create(0, data, len)
+    }
+  }
+}
+
+// File: contracts/lib/Ownable.sol
+
+/**
+ * @title Ownable
+ * @dev The Ownable contract has an owner address, and provides basic authorization control
+ * functions, this simplifies the implementation of "user permissions".
+ */
+contract Ownable {
+  address public owner;
+
+  event OwnershipRenounced(address indexed previousOwner);
+  event OwnershipTransferred(
+    address indexed previousOwner,
+    address indexed newOwner
+  );
+
+  /**
+   * @dev The Ownable constructor sets the original `owner` of the contract to the sender
+   * account.
+   */
+  constructor() public {
+    owner = msg.sender;
+  }
+
+  /**
+   * @dev Throws if called by any account other than the owner.
+   */
+  modifier onlyOwner() {
+    require(msg.sender == owner);
+    _;
+  }
+
+  /**
+   * @dev Allows the current owner to relinquish control of the contract.
+   * @notice Renouncing to ownership will leave the contract without an owner.
+   * It will not be possible to call the functions with the `onlyOwner`
+   * modifier anymore.
+   */
+  function renounceOwnership() public onlyOwner {
+    emit OwnershipRenounced(owner);
+    owner = address(0);
+  }
+
+  /**
+   * @dev Allows the current owner to transfer control of the contract to a newOwner.
+   * @param _newOwner The address to transfer ownership to.
+   */
+  function transferOwnership(address _newOwner) public onlyOwner {
+    _transferOwnership(_newOwner);
+  }
+
+  /**
+   * @dev Transfers control of the contract to a newOwner.
+   * @param _newOwner The address to transfer ownership to.
+   */
+  function _transferOwnership(address _newOwner) internal {
+    require(_newOwner != address(0));
+    emit OwnershipTransferred(owner, _newOwner);
+    owner = _newOwner;
+  }
+}
+
+// File: contracts/lib/SafeMath.sol
+
+/**
+ * @title SafeMath
+ * @dev Math operations with safety checks that throw on error
+ */
+library SafeMath {
+
+  /**
+  * @dev Multiplies two numbers, throws on overflow.
+  */
+  function mul(uint256 a, uint256 b) internal pure returns (uint256 c) {
+    if (a == 0) {
+      return 0;
+    }
+    c = a * b;
+    assert(c / a == b);
+    return c;
+  }
+
+  /**
+  * @dev Integer division of two numbers, truncating the quotient.
+  */
+  function div(uint256 a, uint256 b) internal pure returns (uint256) {
+    // assert(b > 0); // Solidity automatically throws when dividing by 0
+    // uint256 c = a / b;
+    // assert(a == b * c + a % b); // There is no case in which this doesn't hold
+    return a / b;
+  }
+
+  /**
+  * @dev Subtracts two numbers, throws on overflow (i.e. if subtrahend is greater than minuend).
+  */
+  function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+    assert(b <= a);
+    return a - b;
+  }
+
+  /**
+  * @dev Adds two numbers, throws on overflow.
+  */
+  function add(uint256 a, uint256 b) internal pure returns (uint256 c) {
+    c = a + b;
+    assert(c >= a);
+    return c;
+  }
+}
+
+// File: contracts/cloneFactory/TokenSaleCloneFactory.sol
+
+contract TokenSaleCloneFactory is Ownable, CloneFactory {
+    using SafeMath for uint256;
+    // TokenSale contract address for cloning purposes
+    address public libraryAddress;
+    address starToken;
+
+    mapping(address => bool) public isInstantiation;
+    mapping(address => address[]) public instantiations;
+
+    event ContractInstantiation(address msgSender, address instantiation);
+
+    // 
+    /**
+    * @dev set TokenSale contract clone as well as starToken upon deployment
+    * @param _libraryAddress TokenSale contract address for cloning purposes
+    * @param _starToken Star contract address
+    * in the _libraryAddress deployment
+    */
+    constructor(address _libraryAddress, address _starToken) public {
+        require(_libraryAddress != address(0) && _starToken != address(0), "params _libraryAddress and _starToken should not be empty");
+        libraryAddress = _libraryAddress;
+        starToken = _starToken;
+    }
+
+   /**
+    * @dev Have the option of updating the TokenSale contract for cloning
+    * @param _libraryAddress Address for new contract
+    */
+    function setLibraryAddress(address _libraryAddress) external onlyOwner {
+        require(_libraryAddress != address(0), "params _libraryAddress should not be empty");
+        libraryAddress = _libraryAddress;
+    }
+
+    /**
+     * @dev Returns number of instantiations by creator.
+     * @param creator Contract creator.
+     * @return Returns number of instantiations by creator.
+     */
+    function getInstantiationCount(address creator)
+        public
+        view
+        returns (uint256)
+    {
+        return instantiations[creator].length;
+    }
+
+    /**
+     * @dev Allows verified creation of pools.
+     * @param _startTime The timestamp of the beginning of the crowdsale
+     * @param _endTime Timestamp when the crowdsale will finish
+     * @param _whitelist contract containing the whitelisted addresses
+     * @param _companyToken ERC20 CompanyToken contract address
+     * @param _rate The token rate per ETH
+     * @param _starRate The token rate per STAR
+     * @param _wallet Multisig wallet that will hold the crowdsale funds.
+     * @param _crowdsaleCap Cap for the token sale
+     */
+    function create
+    (
+        uint256 _startTime,
+        uint256 _endTime,
+        address _whitelist,
+        address _companyToken,
+        uint256 _rate,
+        uint256 _starRate,
+        address _wallet,
+        uint256 _crowdsaleCap
+    )
+        public
+    {
+        address tokenSale = createClone(libraryAddress);
+        TokenSaleInterface(tokenSale).init(
+            _startTime,
+            _endTime,
+            _whitelist,
+            starToken,
+            _companyToken,
+            _rate,
+            _starRate,
+            _wallet,
+            _crowdsaleCap
+        );
+
+        register(tokenSale);
+    }
+
+    /**
+     * @dev Registers contract in factory registry.
+     * @param instantiation Address of contract instantiation.
+     */
+    function register(address instantiation)
+        internal
+    {
+        isInstantiation[instantiation] = true;
+        instantiations[msg.sender].push(instantiation);
+
+        emit ContractInstantiation(msg.sender, instantiation);
+    }
+}
