f112a90820b22482418d503ac7b60656cc283d0f
Gustavo Guimaraes <gustavoguimaraes@gmail.com>
2018-08-30 11:55:45 +0200

    add CompanyToken to flat

--- scripts/flatten.sh
+++ scripts/flatten.sh
@@ -5,3 +5,4 @@ rm -rf flats/*
 ./node_modules/.bin/truffle-flattener contracts/StarStaking.sol > flats/StarStaking.sol
 ./node_modules/.bin/truffle-flattener contracts/cloneFactory/TokenSaleCloneFactory.sol > flats/TokenSaleCloneFactory.sol
 ./node_modules/.bin/truffle-flattener contracts/TokenSale.sol > flats/TokenSale.sol
+./node_modules/.bin/truffle-flattener contracts/CompanyToken.sol > flats/CompanyToken.sol
