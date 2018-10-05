db91041e75d3e2f201ce47e9321220e5908cb58c
Gustavo Guimaraes <gustavoguimaraes@gmail.com>
2018-10-05 10:41:35 +0200

    add flattened contract for Whitelist

--- scripts/flatten.sh
+++ scripts/flatten.sh
@@ -6,3 +6,4 @@ rm -rf flats/*
 ./node_modules/.bin/truffle-flattener contracts/cloneFactory/TokenSaleCloneFactory.sol > flats/TokenSaleCloneFactory.sol
 ./node_modules/.bin/truffle-flattener contracts/TokenSale.sol > flats/TokenSale.sol
 ./node_modules/.bin/truffle-flattener contracts/CompanyToken.sol > flats/CompanyToken.sol
+./node_modules/.bin/truffle-flattener contracts/Whitelist.sol > flats/Whitelist.sol
