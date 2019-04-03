d4d47fd0e521120535b37dc585e3d07b52072ef7
Markus Waas <mail@markuswaas.de>
2019-04-03 20:37:51 +1300

    729 update StandardToken with require messages

--- contracts/lib/StandardToken.sol
+++ contracts/lib/StandardToken.sol
@@ -23,9 +23,9 @@ contract StandardToken is ERC20, BasicToken {
    * @param _value uint256 the amount of tokens to be transferred
    */
   function transferFrom(address _from, address _to, uint256 _value) public returns (bool) {
-    require(_to != address(0));
-    require(_value <= balances[_from]);
-    require(_value <= allowed[_from][msg.sender]);
+    require(_to != address(0), 'To address cannot be 0!');
+    require(_value <= balances[_from], 'From user has not enough funds!');
+    require(_value <= allowed[_from][msg.sender], 'From user has not enough approved funds!');
 
     balances[_from] = balances[_from].sub(_value);
     balances[_to] = balances[_to].add(_value);
