89d988a02048f7e965f332997dcfc2366bd3c018
Markus Waas <mail@markuswaas.de>
2019-05-09 10:46:57 +1300

    761 fix critical bug + simplify getSortedSpot usage
    * fix bug of user being multiple times in the top ranks, because hes not removed before inserting again
    * getSortedSpot can now be called with new added stake and computes staking points on the spot
    * some smaller refactorings in here, mainly sticking to max-line of 80

--- contracts/StarStaking.sol
+++ contracts/StarStaking.sol
@@ -25,7 +25,6 @@ contract StarStaking is StarStakingInterface, Lockable {
     mapping (address => uint256) public totalStakedFor;
 
     LinkedListLib.LinkedList topRanks;
-    uint256 public topRanksCount;
 
     uint256 public startTime;
     uint256 public closingTime;
@@ -61,7 +60,7 @@ contract StarStaking is StarStakingInterface, Lockable {
      * @param _tokenOnSale Token that will be sold
      * @param _topRanksMaxSize Maximal size of the top ranks
      * @param _startTime Timestamp for the beginning of the staking event
-     * @param _closingTime Timestamp of the end of staking event
+     * @param _endTime Timestamp of the end of staking event
      * @param _targetRateInEth The baseline target rate in ETH for purchases
      * @param _maxDiscountPer1000 The max discount per 1000 for rank 1.
      * @param _declinePerRankPer1000 The discount decline per rank per 1000.
@@ -74,7 +73,7 @@ contract StarStaking is StarStakingInterface, Lockable {
         ERC20 _starToken,
         ERC20 _tokenOnSale,
         uint256 _startTime,
-        uint256 _closingTime,
+        uint256 _endTime,
         uint256 _topRanksMaxSize,
         uint256 _targetRateInEth,
         uint256 _maxDiscountPer1000,
@@ -83,21 +82,47 @@ contract StarStaking is StarStakingInterface, Lockable {
         uint256 _maxStakePerUser,
         address _wallet
     ) public {
-        require(address(_starEthRateInterface) != address(0), "StarEthRate address must be defined!");
-        require(address(_starToken) != address(0), "Star token address must be defined!");
-        require(address(_tokenOnSale) != address(0), "Token on sale address must be defined!");
-        require(_startTime < _closingTime, "Start time must be before closing time!");
+        require(
+            address(_starEthRateInterface) != address(0),
+            "StarEthRate address must be defined!"
+        );
+        require(
+            address(_starToken) != address(0),
+            "Star token address must be defined!"
+        );
+        require(
+            address(_tokenOnSale) != address(0),
+            "Token on sale address must be defined!"
+        );
+        require(
+            _startTime < _endTime,
+            "Start time must be before closing time!"
+        );
         require(_startTime >= now, "Start time must be after current time!");
         require(_topRanksMaxSize > 0, "Top ranks size must be more than 0!");
         require(_targetRateInEth > 0, "Target rate must be more than 0!");
         require(_maxDiscountPer1000 > 0, "Max discount must be more than 0!");
-        require(_declinePerRankPer1000 > 0, "Decline per rank must be more than 0!");
-        require(_stakeSaleCap > 0, "StakingSale cap should be higher than 0!");
-        require(_maxStakePerUser > 0, "Max stake per user should be higher than 0!");
-        require(_maxStakePerUser < _stakeSaleCap, "Max stake per user should be smaller than StakeSale cap!");
+        require(
+            _declinePerRankPer1000 > 0,
+            "Decline per rank must be more than 0!"
+        );
+        require(
+            _stakeSaleCap > 0,
+            "StakingSale cap should be higher than 0!"
+        );
+        require(
+            _maxStakePerUser > 0,
+            "Max stake per user should be higher than 0!"
+        );
+        require(
+            _maxStakePerUser < _stakeSaleCap,
+            "Max stake per user should be smaller than StakeSale cap!"
+        );
         require(_wallet != address(0), "Wallet address may must be defined!");
 
-        uint256 maxDecline = _topRanksMaxSize.sub(1).mul(_declinePerRankPer1000);
+        uint256 maxDecline = _topRanksMaxSize
+            .sub(1)
+            .mul(_declinePerRankPer1000);
 
         require(
             _maxDiscountPer1000 >= maxDecline,
@@ -108,8 +133,7 @@ contract StarStaking is StarStakingInterface, Lockable {
         starToken = _starToken;
         tokenOnSale = _tokenOnSale;
         startTime = _startTime;
-        closingTime = _closingTime;
-        topRanksCount = 0;
+        closingTime = _endTime;
         topRanksMaxSize = _topRanksMaxSize;
         targetRateInEth = _targetRateInEth;
         maxDiscountPer1000 = _maxDiscountPer1000;
@@ -122,19 +146,19 @@ contract StarStaking is StarStakingInterface, Lockable {
     /**
      * @dev Stakes a certain amount of tokens.
      * @param _amount Amount of tokens to stake.
-     * @param _node Node as reference for insert position into top ranks.
+     * @param _oneRankAboveNode Node as reference for insert position into top ranks.
      */
-    function stake(uint256 _amount, address _node) public {
-        stakeFor(msg.sender, _amount, _node);
+    function stake(uint256 _amount, address _oneRankAboveNode) public {
+        stakeFor(msg.sender, _amount, _oneRankAboveNode);
     }
 
     /**
      * @dev Stakes a certain amount of tokens for another user.
      * @param _user Address of the user to stake for.
      * @param _amount Amount of tokens to stake.
-     * @param _node Node as reference for insert position into top ranks.
+     * @param _oneRankAboveNode Node as reference for insert position into top ranks.
      */
-    function stakeFor(address _user, uint256 _amount, address _node)
+    function stakeFor(address _user, uint256 _amount, address _oneRankAboveNode)
         public
         onlyWhenUnlocked
         whenStakingOpen
@@ -158,22 +182,22 @@ contract StarStaking is StarStakingInterface, Lockable {
             amount = maxStakePerUser.sub(totalStakedFor[_user]);
         }
 
-        if (topRanksCount > 0) {
+        if (topRanks.sizeOf() > 0) {
             require(
-                _node == HEAD || topRanks.nodeExists(_node),
+                _oneRankAboveNode == HEAD || topRanks.nodeExists(_oneRankAboveNode),
                 "Node for suggested position does not exist!"
             );
         } else {
             require(
-                _node == HEAD,
+                _oneRankAboveNode == HEAD,
                 "Reference node must be empty for first inserted node!"
             );
         }
 
         _addStakingPoints(_user, amount);
 
-        if (topRanksCount == 0 || _node != HEAD) {
-            _sortedInsert(_user, _node);
+        if (topRanks.sizeOf() == 0 || _oneRankAboveNode != HEAD) {
+            _sortedInsert(_user, _oneRankAboveNode);
         }
 
         require(
@@ -185,17 +209,39 @@ contract StarStaking is StarStakingInterface, Lockable {
 
     /**
      * @dev Can be used before `stakeFor` to get the correct reference node
-     * @param _value value to seek
+     * @param _addedStake new added stake for calling user
      * @return reference node for insertion in top ranks
      */
-    function getSortedSpot(uint256 _value) external view returns (address) {
+    function getSortedSpotForNewStake(uint256 _addedStake)
+        external
+        view
+        returns (address)
+    {
+        address user = msg.sender;
+        uint256 newStakingPoints = _computeStakingPoints(user, _addedStake);
+
+        return getSortedSpot(newStakingPoints);
+    }
+
+    /**
+     * @dev Can be used before `stakeFor` to get the correct reference node
+     * @param _stakingPoints new points for user to insert
+     * @return reference node for insertion in top ranks
+     */
+    function getSortedSpot(uint256 _stakingPoints)
+        public
+        view
+        returns (address)
+    {
         if (topRanks.sizeOf() == 0) {
             return HEAD;
         }
 
-        address node = getTopRank(node, PREV);
+        address node = getTopRank(HEAD, PREV);
 
-        while ((node != HEAD) && ((totalStakingPointsFor[node] < _value))) {
+        while (
+            (node != HEAD) && ((totalStakingPointsFor[node] < _stakingPoints))
+        ) {
             node = getTopRank(node, PREV);
         }
 
@@ -224,7 +270,7 @@ contract StarStaking is StarStakingInterface, Lockable {
      * @dev Returns a flat list of 3-tuples (address, stakingPoints, totalStaked).
      */
     function getTopRanksTuples() public view returns (uint256[]) {
-        uint256 tripleRanksCount = topRanksCount * 3;
+        uint256 tripleRanksCount = topRanks.sizeOf() * 3;
         uint256[] memory topRanksList = new uint256[](tripleRanksCount);
 
         address referenceNode = HEAD;
@@ -275,10 +321,14 @@ contract StarStaking is StarStakingInterface, Lockable {
             .div(decimalCorrectionFactor);
     }
 
-    function _computeBonusTokens(uint256 _baseTokens) private view returns (uint256) {
+    function _computeBonusTokens(uint256 _baseTokens)
+        private
+        view
+        returns (uint256)
+    {
         address referenceNode = HEAD;
 
-        for (uint256 i = 0; i < topRanksCount; i++) {
+        for (uint256 i = 0; i < topRanks.sizeOf(); i++) {
             referenceNode = getTopRank(referenceNode, NEXT);
             
             if (referenceNode == msg.sender) {
@@ -292,15 +342,22 @@ contract StarStaking is StarStakingInterface, Lockable {
         return 0;
     }
 
-    function _addStakingPoints(address _user, uint256 _amount) private {
+    function _computeStakingPoints(address _user, uint256 _amount)
+        private
+        view
+        returns (uint256)
+    {
         uint256 timeUntilEnd = closingTime.sub(now);
         uint256 addedStakingPoints = timeUntilEnd.mul(_amount);
 
-        totalStakingPointsFor[_user] = totalStakingPointsFor[_user]
-            .add(addedStakingPoints);
+        return totalStakingPointsFor[_user].add(addedStakingPoints);
+    }
+
+    function _addStakingPoints(address _user, uint256 _amount) private {
+        totalStakingPointsFor[_user] = _computeStakingPoints(_user, _amount);
         totalStakedFor[_user] = totalStakedFor[_user].add(_amount);
 
-        emit Staked(_user, _amount, addedStakingPoints);
+        emit Staked(_user, _amount);
     }
 
     function _doesCorrectlyInsertAtFirstRank(
@@ -308,7 +365,7 @@ contract StarStaking is StarStakingInterface, Lockable {
         uint256 _oneRankAbovePoints,
         address _twoRanksAbove
     ) private view returns (bool) {
-        if (topRanksCount == 0) {
+        if (topRanks.sizeOf() == 0) {
             return true;
         }
 
@@ -330,14 +387,15 @@ contract StarStaking is StarStakingInterface, Lockable {
         );
     }
 
-    function _sortedInsert(address _user, address _node) private {
-        address oneRankAbove = _node;
-        address twoRanksAbove = getTopRank(oneRankAbove, PREV);
-        address oneRankBelow = getTopRank(_node, NEXT);
+    function _sortedInsert(address _user, address _oneRankAboveNode) private {
+        topRanks.remove(_user);
+
+        address twoRanksAbove = getTopRank(_oneRankAboveNode, PREV);
+        address oneRankBelow = getTopRank(_oneRankAboveNode, NEXT);
 
         uint256 newRankPoints = totalStakingPointsFor[_user];
         uint256 oneRankBelowPoints = totalStakingPointsFor[oneRankBelow];
-        uint256 oneRankAbovePoints = totalStakingPointsFor[oneRankAbove];
+        uint256 oneRankAbovePoints = totalStakingPointsFor[_oneRankAboveNode];
 
         if (_doesCorrectlyInsertAtFirstRank(
             newRankPoints,
@@ -352,15 +410,11 @@ contract StarStaking is StarStakingInterface, Lockable {
                 oneRankAbovePoints
             );
 
-            topRanks.insert(_node, _user, NEXT);
+            topRanks.insert(_oneRankAboveNode, _user, NEXT);
         }
-        
-        topRanksCount < topRanksMaxSize
-            ? _incrementTopRanksCount() : topRanks.pop(PREV);
-    }
 
-    function _incrementTopRanksCount() private returns (address) {
-        topRanksCount = topRanksCount.add(1);
-        return HEAD;
+        if (topRanks.sizeOf() >= topRanksMaxSize) {
+            topRanks.pop(PREV);
+        }
     }
 }
--- contracts/StarStakingInterface.sol
+++ contracts/StarStakingInterface.sol
@@ -1,5 +1,5 @@
 pragma solidity 0.4.25;
 
 contract StarStakingInterface {
-    event Staked(address indexed user, uint256 amount, uint256 addedStakingPoints);
+    event Staked(address indexed user, uint256 amount);
 }
